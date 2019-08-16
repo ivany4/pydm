48dd1287a2dff5156822f34ed3c1f2d1b636ca74
Ivan Sinkarenko <ivan.sinkarenko@cern.ch>
2019-08-16 11:28:03 +0200

    ENH: Initializing data plugins only on demand to avoid circular import problems

--- pydm/widgets/template_repeater.py
+++ pydm/widgets/template_repeater.py
@@ -9,9 +9,10 @@ from qtpy.QtCore import Qt, QSize, QRect, Property, QPoint, Q_ENUMS
 from .base import PyDMPrimitiveWidget
 from pydm.utilities import is_qt_designer
 import pydm.data_plugins
-from ..utilities import macro, find_file
+from ..utilities import find_file
 from ..display import load_file
 logger = logging.getLogger(__name__)
+pydm.data_plugins.initialize_plugins_if_needed()
 
 
 class FlowLayout(QLayout):
